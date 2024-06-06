c340d88b1d0563faa63e04b35b3bc297e01d80a1
kawtar <kawtarennabirha@gmail.com>
2024-06-06 11:44:05 +0100

    task0

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+"""Module documentation"""
+
+
 def add(a: float, b: float) -> float:
-    " takes a float a and a float b and return their sum"
+    """takes a float a and a float b and return their sum"""
     return a + b

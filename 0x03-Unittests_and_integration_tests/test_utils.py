c07660cf90130632e0767c792025a96c76876386
kawtar <kawtarennabirha@gmail.com>
2024-07-03 16:20:47 +0100

    task0

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+" doc doc"
+import unittest
+from parameterized import parameterized # type: ignore
+from utils import access_nested_map, get_json, memoize
+from typing import Any, Tuple, Dict
+from unittest.mock import patch, Mock
+
+class TestAccessNestedMap (unittest.TestCase):
+    "doc doc"
+    @parameterized.expand(
+        [
+            ({"a": 1}, ("a",), 1),
+            ({"a": {"b": 2}}, ("a",), {"b": 2}),
+            ({"a": {"b": 2}}, ("a", "b"), 2),
+        ]
+    )
+    def test_access_nested_map(
+        self, nested_map: Dict[str, Any], path: Tuple[str], expected: Any
+    ) -> None:
+        """doc doc doc"""
+        self.assertEqual(access_nested_map(nested_map, path), expected)
